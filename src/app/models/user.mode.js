17dde2deee5f6c9adbd99798fbd085a44b751aba
Giovani Cassiano Nogueira <32183475+giovaninogueira@users.noreply.github.com>
2021-12-29 15:51:14 -0300

    chore: removendo comentários

--- src/app/models/user.mode.js
+++ src/app/models/user.mode.js
@@ -2,17 +2,13 @@ import { DataTypes } from "sequelize"
 import { sequelize } from "../../config/database.js";
 
 const UserEntity = sequelize.define('User', {
-  // Model attributes are defined here
   firstName: {
     type: DataTypes.STRING,
     allowNull: false
   },
   lastName: {
     type: DataTypes.STRING
-    // allowNull defaults to true
   }
-}, {
-  // Other model options go here
 });
 
 export default UserEntity;
--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,3 @@
-// Option 2: Passing parameters separately (sqlite)
 import { Sequelize } from "sequelize";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
@@ -7,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 /**
- * Connect DB
+ * Connect DB Sequelize
  */
 const sequelize = new Sequelize({
   dialect: "sqlite",
